03bf62ddcdddb48ded5fff4470f7ebafda36533b
Marcel Roa <mroa@limetec-biotechnologies.de>
2017-09-29 15:31:52 +0200

    added google maps support

--- www/js/main.js
+++ www/js/main.js
@@ -1,7 +1,15 @@
-function onDeviceReady() {
-    alert("Muh");
-    var div = document.getElementById("map_canvas");
+document.addEventListener("deviceready", function() {
+    alert("muh");
+   var div = document.getElementById("map");
 
     // Initialize the map view
-    var map = plugin.google.maps.Map.getMap(div);
+var  map = plugin.google.maps.Map.getMap(div);
+
+    // Wait until the map is ready status.
+    map.addEventListener(plugin.google.maps.event.MAP_READY, onMapReady);
+}, false);
+
+
+function onMapReady() {
+   alert("yeha");
 }
