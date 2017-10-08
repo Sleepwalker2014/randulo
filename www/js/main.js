81c5e4e46002fa0c1f016046078453ace32a3403
marcel <marcel.roa@gmx.de>
2017-10-08 20:58:37 +0200

    added static navbar

--- www/js/main.js
+++ www/js/main.js
@@ -5,8 +5,23 @@ var  map = plugin.google.maps.Map.getMap(div);
 
     // Wait until the map is ready status.
     map.addEventListener(plugin.google.maps.event.MAP_READY, onMapReady);
+
+    element1 = document.getElementById("overlayDiv");
+    element1.addEventListener("click", fullscreen)
 }, false);
 
 
 function onMapReady() {
+
+}
+
+function fullscreen () {
+    var height = '100px';
+    element1 = document.getElementById("overlayDiv");
+    var currentHeight = element1.style.height;
+
+    if (currentHeight == "100px") {
+        height = '100%';
+    }
+    element1.style.height = height;
 }
