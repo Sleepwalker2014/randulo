69546bb0651ae5e750655fad3865d67dbcd7ecf1
Marcel Roa <mroa@limetec-biotechnologies.de>
2017-10-16 16:09:06 +0200

    changed a few thingso

--- www/js/main.js
+++ www/js/main.js
@@ -1,27 +1,27 @@
-document.addEventListener("deviceready", function() {
-var div = document.getElementById("map");
+document.addEventListener("deviceready", function () {
+    var div = document.getElementById("map");
     // Initialize the map view
-var  map = plugin.google.maps.Map.getMap(div);
+    var map = plugin.google.maps.Map.getMap(div);
 
     // Wait until the map is ready status.
     map.addEventListener(plugin.google.maps.event.MAP_READY, onMapReady);
 
-    element1 = document.getElementById("overlayDiv");
-    element1.addEventListener("click", fullscreen)
+    element2 = document.getElementById("bomb");
+    element2.addEventListener("click", hide);
+
+    $(".doubleLineHeight").mousemove(function (e) {
+        $(this).height(e.pageY);
+    });
 }, false);
 
-
-function onMapReady() {
+function onMapReady () {
 
 }
 
-function fullscreen () {
-    var height = '100px';
-    element1 = document.getElementById("overlayDiv");
-    var currentHeight = element1.style.height;
-
-    if (currentHeight == "100px") {
-        height = '100%';
-    }
-    element1.style.height = height;
+function hide () {
+    $("#inputFields").animate({
+        height: "toggle"
+    }, 400, function () {
+        // Animation complete.
+    });
 }
